f4f96d1bfcd846e1c0ddb0c669534bffdd3c0ec9
Patrick Bär <pb@dc.ag>
2022-08-26 21:48:23 +0200

    finished tutorial

--- index.js
+++ index.js
@@ -14,16 +14,35 @@ const socketIO = new Server(http, {
 	},
 });
 
+let users = [];
+
 socketIO.on("connection", (socket) => {
 	console.log(`⚡: ${socket.id} user just connected!`);
 
 	socket.on("message", (data) => {
 		console.log(data);
 		socketIO.emit("messageResponse", data);
+		socket.broadcast.emit('typingResponse', '');
+	});
+
+	socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+
+	socket.on('newUser', (data) => {
+		//Adds the new user to the list of users
+		users.push(data);
+		// console.log(users);
+		//Sends the list of users to the client
+		socket.emit('newUserResponse', users);
 	});
 
 	socket.on("disconnect", () => {
 		console.log("🔥: A user disconnected");
+		//Updates the list of users when a user disconnects from the server
+		users = users.filter((user) => user.socketID !== socket.id);
+		// console.log(users);
+		//Sends the list of users to the client
+		socketIO.emit('newUserResponse', users);
+		socket.disconnect();
 	});
 });
 
